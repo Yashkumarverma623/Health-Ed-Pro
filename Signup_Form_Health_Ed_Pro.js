30a26b2e0ea999ddb2a5191de60f139f30305892
Yash Kumar Verma <yashkumarverma623@gmail.com>
2024-10-15 09:45:50 +0530

    Add files via upload
    Login and Sign Up form

---
+++ Signup_Form_Health_Ed_Pro.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector('.wrapper');
+const signuplink = document.querySelector('.signup-link');
+const loginlink = document.querySelector('.login-link');
+
+signuplink.onclick = () => {
+  wrapper.classList.add('active');
+}
