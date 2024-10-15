b0a89fdc75fa269d1c7c138238ab8e11fb68e438
Yash Kumar Verma <yashkumarverma623@gmail.com>
2024-10-15 21:27:16 +0530

    Update Signup_Form_Health_Ed_Pro.js
    Login Page

--- Signup_Form_Health_Ed_Pro.js
+++ Signup_Form_Health_Ed_Pro.js
@@ -5,3 +5,7 @@ const loginlink = document.querySelector('.login-link');
 signuplink.onclick = () => {
   wrapper.classList.add('active');
 }
+
+loginlink.onclick = () => {
+  wrapper.classList.remove('active');
+}
